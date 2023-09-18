90dabd901459095b39726e3db0b5952917cc81e0
Nores <70499515+EspoirNorbert@users.noreply.github.com>
2023-09-18 06:08:58 +0200

    Mise en place du service pokemon

--- src/app/pokemon/pokemon.service.ts
+++ src/app/pokemon/pokemon.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { Pokemon } from './pokemon';
+import { POKEMONS } from './mock-pockemon';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
 
-  constructor() { }
+  getPokemonList(): Pokemon[] {
+    return POKEMONS;
+  }
+
+  getPokemonById(pokemonId: number): Pokemon|undefined {
+    return POKEMONS.find(pokemon => pokemon.id== pokemonId)
+  }
+
+  getPokemonTypeList(): string[] {
+    return [
+      'Plante', 
+      'Feu', 
+      'Eau', 
+      'Insecte',
+      'Normal',
+      'Electrik', 
+      'Poison', 
+      'Fée',
+      'Vol',
+      'Combat',
+      'Psy'
+    ];
+  }
 }
