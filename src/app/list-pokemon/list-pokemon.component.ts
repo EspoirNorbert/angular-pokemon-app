51a8d2388ffc1c865be04ff374b24d8b1d27f332
Nores <70499515+EspoirNorbert@users.noreply.github.com>
2023-09-18 05:35:39 +0200

    Implementation de la route vers les details

--- src/app/list-pokemon/list-pokemon.component.ts
+++ src/app/list-pokemon/list-pokemon.component.ts
@@ -1,6 +1,7 @@
+import { Pokemon } from './../pokemon';
 import { Component } from '@angular/core';
-import { Pokemon } from '../pokemon';
 import { POKEMONS } from '../mock-pockemon';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list-pokemon',
@@ -12,6 +13,8 @@ export class ListPokemonComponent {
 
   pokemonList: Pokemon[] = POKEMONS;
   pokemonSelected: Pokemon | undefined;
+
+  constructor(private router : Router) {}
 
   ngOnInit() {
     console.table(this.pokemonList);
@@ -29,5 +32,9 @@ export class ListPokemonComponent {
       console.log(`Vous avez demandé un pokémon qui n'existe pas.`);
       this.pokemonSelected = pokemon;
     }
+  }
+
+  goToPokemon(pokemon: Pokemon){
+    this.router.navigate(['/pokemons', pokemon.id])
   }
 }
