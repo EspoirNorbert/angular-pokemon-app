b7ae0636f88bdd415b6ecba0afeca9e60d9a618b
Nores <70499515+EspoirNorbert@users.noreply.github.com>
2023-09-18 05:40:30 +0200

    Ajout de la barre de navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,16 @@
-import { Component } from '@angular/core';
-import { Pokemon } from './pokemon';
-import { POKEMONS } from './mock-pockemon';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styles: []
+  selector: "app-root",
+  template: `
+    <nav>
+      <div class="nav-wrapper teal">
+        <a routerLink="/pokemons" class="brand-logo center"> Pokédex </a>
+      </div>
+    </nav>
+
+    <router-outlet></router-outlet>
+  `,
+  styles: [],
 })
-export class AppComponent {
-  pokemonList: Pokemon[] = POKEMONS;
-  pokemonSelected: Pokemon | undefined;
-
-  ngOnInit() {
-    console.table(this.pokemonList);
-  }
-
-  selectPokemon(pokemonId: string) {
-    const pokemon: Pokemon | undefined = this.pokemonList.find(
-      (pokemon) => pokemon.id == +pokemonId
-    );
-
-    if (pokemon) {
-      console.log(`Vous avez demandé sur le pokémon ${pokemon.name}`);
-      this.pokemonSelected = pokemon;
-    } else {
-      console.log(`Vous avez demandé un pokémon qui n'existe pas.`);
-      this.pokemonSelected = pokemon;
-    }
-  }
-}
+export class AppComponent {}
