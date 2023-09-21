99e879a8b69c867c1a14fabeca21cd7dac3477d0
Nores <70499515+EspoirNorbert@users.noreply.github.com>
2023-09-21 16:28:33 +0200

    Refactoring des methodes du pokemon.service.ts en integrant les observables

--- src/app/pokemon/pokemon.service.ts
+++ src/app/pokemon/pokemon.service.ts
@@ -2,18 +2,31 @@ import { Injectable } from '@angular/core';
 import { Pokemon } from './pokemon';
 import { POKEMONS } from './mock-pockemon';
 import { HttpClient } from '@angular/common/http';
+import { tap,Observable, catchError,of } from 'rxjs';
 
 @Injectable()
 export class PokemonService {
   
   constructor(private http: HttpClient) {}
 
-  getPokemonList(): Pokemon[] {
-    return POKEMONS;
+  getPokemonList(): Observable<Pokemon[]>  {
+    return this.http.get<Pokemon[]>('api/pokemons').pipe(
+      tap ((pokemonList) => console.table(pokemonList)),
+      catchError((error)=> {
+        console.log(error);
+        return of([])
+      })
+    )
   }
 
-  getPokemonById(pokemonId: number): Pokemon|undefined {
-    return POKEMONS.find(pokemon => pokemon.id== pokemonId)
+  getPokemonById(pokemonId: number): Observable<Pokemon|undefined> {
+    return this.http.get<Pokemon>(`api/pokemons/${pokemonId}`).pipe(
+      tap ((pokemonList) => console.table(pokemonList)),
+      catchError((error)=> {
+        console.log(error);
+        return of(undefined)
+      })
+    )
   }
 
   getPokemonTypeList(): string[] {
