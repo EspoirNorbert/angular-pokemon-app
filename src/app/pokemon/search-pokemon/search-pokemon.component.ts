afba07684e96ed5b893e7e8ec005145a8b67f1c8
Nores <70499515+EspoirNorbert@users.noreply.github.com>
2023-09-27 18:10:25 +0200

    Ajout des methodes reactives de recherche et creation du template search

--- src/app/pokemon/search-pokemon/search-pokemon.component.ts
+++ src/app/pokemon/search-pokemon/search-pokemon.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from '../pokemon';
+import { Route, Router } from '@angular/router';
+import { Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-search-pokemon',
   templateUrl: './search-pokemon.component.html',
-  styles: [
-  ]
 })
-export class SearchPokemonComponent {
+export class SearchPokemonComponent implements OnInit {
+  // {..."a" , ..."ab".... "abz"}
+  searchTerms = new Subject<string>();
+  // {...pokemonList(a)....pokemonList(ab)}
+  pokemons$: Observable<Pokemon[]>;
 
+  /***
+   * Suject appartient à rxJS elle stocke les requettes suscevices
+   * Pour obtenir un flux de données dans le temps;
+   * 
+   * Un subject peut piloter un observable
+   * Construire un flux de données.
+   */
+
+  constructor(private router: Router){}
+
+  ngOnInit(): void {}
+
+  search (term: string){
+    // pousser le terme de recherche 
+    // next comme push 
+    this.searchTerms.next(term);
+  }
+
+  goToDetail(pokemon: Pokemon){
+    const link = ['/pokemon' , pokemon.id]
+    this.router.navigate(link);
+  }
 }
